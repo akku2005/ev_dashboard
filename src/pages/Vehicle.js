f67954f7b3d27aa9e8539012b3829c735fafc192
akku2005 <asakashsahu20@gmail.com>
2024-04-16 01:07:00 +0530

    vehicels

--- src/pages/Vehicle.js
+++ src/pages/Vehicle.js
@@ -1,6 +1,7 @@
 import React from "react";
 import AdminSidebar from "../components/Dashboard/SideNavbar/AdminSidebar";
 import TopNavbar from "../components/Dashboard/TopNavbar";
+import bikeImg from "../assets/images/scootey1.png";
 
 const Vehicle = () => {
   return (
@@ -11,7 +12,24 @@ const Vehicle = () => {
           <div className="bar">
             <TopNavbar />
           </div>
-          <section></section>
+          <section className="grid grid-cols-1 lg:grid-cols-2 gap-4 mt-10 mr-5">
+            <div>
+              <div className="bg-lime-500 p-4 mb-2">
+                {/* Adjusted padding for larger image */}
+                <div className="w-full h-full p-20">
+                  <img src={bikeImg} alt="bike-image" style={{ width: "100%", height: "auto" }} />
+                  <div className="bg-red-500 rounded-full w-24 h-24 flex items-center justify-center">
+                    <div className="text-white">Akash</div>
+                  </div>
+                </div>
+              </div>
+              <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+                <div className="bg-gray-200 p-4">Left</div>
+                <div className="bg-gray-200 p-4">Right</div>
+              </div>
+            </div>
+            <div className="bg-gray-200 p-4">Sahu</div>
+          </section>
         </main>
       </div>
     </div>
